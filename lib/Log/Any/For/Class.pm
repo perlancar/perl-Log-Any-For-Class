8fc4dbe984001315ae328069dd416387db14a596
Steven Haryanto (on Asus EEEPC) <stevenharyanto@gmail.com>
2012-07-10 11:47:43 +0700

    Add Log::Any::For::Package

--- lib/Log/Any/For/Class.pm
+++ lib/Log/Any/For/Class.pm
@@ -7,190 +7,72 @@ use Log::Any '$log';
 
 # VERSION
 
+use Data::Clone;
 use Scalar::Util qw(blessed);
 # doesn't currently work, Log::Log4perl not fooled
 #use Sub::Uplevel;
 
 our %SPEC;
 require Exporter;
-our @ISA = qw(Exporter);
+use Log::Any::For::Package qw(add_logging_to_package);
+our @ISA = qw(Log::Any::For::Package Exporter);
 our @EXPORT_OK = qw(add_logging_to_class);
 
-# XXX copied from SHARYANTO::Package::Util
-sub package_exists {
-    no strict 'refs';
-
-    my $pkg = shift;
-
-    return unless $pkg =~ /\A\w+(::\w+)*\z/;
-    if ($pkg =~ s/::(\w+)\z//) {
-        return !!${$pkg . "::"}{$1 . "::"};
-    } else {
-        return !!$::{$pkg . "::"};
-    }
-}
-
-sub _default_precall_logger {
-    my %args  = @_;
-    my $name  = $args{name};
-    my $margs = $args{args};
-
-    #uplevel 2, $args{orig}, @$margs;
-
-    # exclude $self or package
-    my $o = shift @$margs;
-
-    unless (blessed $o) {
-        $name =~ s/::(\w+)$/->$1/;
-    }
-
-    $log->tracef("---> %s(%s)", $name, $margs);
-}
-
-sub _default_postcall_logger {
-    my %args = @_;
-    #uplevel 2, $args{orig}, @{$args{args}};
-
-    $log->tracef("<--- %s() = %s", $args{name}, $args{result});
-}
-
-$SPEC{add_logging_to_class} = {
-    v => 1.1,
-    summary => 'Add logging to class',
-    description => <<'_',
+my $spec = $Log::Any::For::Package::SPEC{add_logging_to_package};
+$spec->{summary} = 'Add logging to class';
+$spec->{description} = <<'_';
 
 Logging will be done using Log::Any.
 
 Currently this function adds logging around method calls, e.g.:
 
-    -> Package::method(...)
-    <- Package::method() = RESULT
+    -> Class::method(...)
+    <- Class::method() = RESULT
     ...
 
 _
-    args => {
-        classes => {
-            summary => 'Classes to add logging to',
-            schema => ['array*' => {of=>'str*'}],
-            req => 1,
-            pos => 0,
-        },
-        precall_logger => {
-            summary => 'Supply custom precall logger',
-            schema  => 'code*',
-            description => <<'_',
-
-Code will be called when logging method call. Code will be given a hash argument
-%args containing these keys: `args` (arrayref, the original @_), `orig`
-(coderef, the original method), `name` (string, the fully-qualified method
-name).
-
-You can use this mechanism to do selective logging, preprocess log message, etc.
-
-_
-        },
-        postcall_logger => {
-            summary => 'Supply custom postcall logger',
-            schema  => 'code*',
-            description => <<'_',
-
-Just like precall_logger, but code will be called after method is call. Code
-will be given a hash argument %args containing these keys: `args` (arrayref, the
-original @_), `orig` (coderef, the original method), `name` (string, the
-fully-qualified method name), `result` (arrayref, the method result).
-
-You can use this mechanism to do selective logging, preprocess log message,
-etc.
-
-_
-        },
-    },
-    result_naked => 1,
+delete $spec->{args}{packages};
+$spec->{args}{classes} = {
+    summary => 'Classes to add logging to',
+    schema => ['array*' => {of=>'str*'}],
+    req => 1,
+    pos => 0,
 };
+$SPEC{add_logging_to_class} = $spec;
 sub add_logging_to_class {
     my %args = @_;
 
     my $classes = $args{classes} or die "Please specify 'classes'";
     $classes = [$classes] unless ref($classes) eq 'ARRAY';
+    delete $args{classes};
 
-    for my $class (@$classes) {
+    $args{precall_logger} //= sub {
+        my %args  = @_;
+        my $name  = $args{name};
+        my $margs = $args{args};
 
-        die "Invalid class name $class" unless $class =~ /\A\w+(::\w+)*\z/;
+        #uplevel 2, $args{orig}, @$margs;
 
-        # require class
-        unless (package_exists($class)) {
-            eval "use $class; 1" or die "Can't load $class: $@";
+        # exclude $self or package
+        my $o = shift @$margs;
+
+        unless (blessed $o) {
+            $name =~ s/::(\w+)$/->$1/;
         }
 
-        my $src;
-        # get the calling package symbol table name
-        {
-            no strict 'refs';
-            $src = \%{ $class . '::' };
-        }
+        $log->tracef("---> %s(%s)", $name, $margs);
+    };
+    $args{postcall_logger} //= sub {
+        my %args = @_;
+        #uplevel 2, $args{orig}, @{$args{args}};
 
-        # loop through all symbols in calling package, looking for subs
-        for my $symbol (keys %$src) {
-            # get all code references, make sure they're valid
-            my $sub = *{ $src->{$symbol} }{CODE};
-            next unless defined $sub and defined &$sub;
+        $log->tracef("<--- %s() = %s", $args{name}, $args{result});
+    };
 
-            # save all other slots of the typeglob
-            my @slots;
-
-            for my $slot (qw( SCALAR ARRAY HASH IO FORMAT )) {
-                my $elem = *{ $src->{$symbol} }{$slot};
-                next unless defined $elem;
-                push @slots, $elem;
-            }
-
-            # clear out the source glob
-            undef $src->{$symbol};
-
-            # replace the sub in the source
-            $src->{$symbol} = sub {
-                my $logger;
-                my @args = @_;
-
-                $logger = $args{precall_logger} // \&_default_precall_logger;
-                $logger->(
-                    orig   => $sub,
-                    name   => "${class}::$symbol",
-                    args   => [@args],
-                );
-
-                my $wa = wantarray;
-                my @res;
-                if ($wa) {
-                    @res =  $sub->(@args);
-                } else {
-                    $res[0] = $sub->(@args);
-                }
-
-                $logger = $args{postcall_logger} // \&_default_postcall_logger;
-                $logger->(
-                    orig   => $sub,
-                    name   => "${class}::$symbol",
-                    args   => [@args],
-                    result => \@res,
-                );
-
-                if ($wa) {
-                    return @res;
-                } else {
-                    return $res[0];
-                }
-            };
-
-            # replace the other slot elements
-            for my $elem (@slots) {
-                $src->{$symbol} = $elem;
-            }
-        } # for $symbol
-
-    } # for $class
-
-    1;
+    add_logging_to_package(
+        %args,
+        packages => $classes,
+    );
 }
 
 1;
---
+++ lib/Log/Any/For/Package.pm
@@ -0,0 +1,206 @@
+package Log::Any::For::Package;
+
+use 5.010;
+use strict;
+use warnings;
+use Log::Any '$log';
+
+# VERSION
+
+use Scalar::Util qw(blessed);
+# doesn't currently work, Log::Log4perl not fooled
+#use Sub::Uplevel;
+
+our %SPEC;
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(add_logging_to_package);
+
+# XXX copied from SHARYANTO::Package::Util
+sub package_exists {
+    no strict 'refs';
+
+    my $pkg = shift;
+
+    return unless $pkg =~ /\A\w+(::\w+)*\z/;
+    if ($pkg =~ s/::(\w+)\z//) {
+        return !!${$pkg . "::"}{$1 . "::"};
+    } else {
+        return !!$::{$pkg . "::"};
+    }
+}
+
+sub _default_precall_logger {
+    my %args = @_;
+    #uplevel 2, $args{orig}, @{$args{args}};
+
+    $log->tracef("---> %s(%s)", $args{name}, $args{args});
+}
+
+sub _default_postcall_logger {
+    my %args = @_;
+    #uplevel 2, $args{orig}, @{$args{args}};
+
+    $log->tracef("<--- %s() = %s", $args{name}, $args{result});
+}
+
+$SPEC{add_logging_to_package} = {
+    v => 1.1,
+    summary => 'Add logging to package',
+    description => <<'_',
+
+Logging will be done using Log::Any.
+
+Currently this function adds logging around function calls, e.g.:
+
+    -> Package::func(...)
+    <- Package::func() = RESULT
+    ...
+
+_
+    args => {
+        packages => {
+            summary => 'Packages to add logging to',
+            schema => ['array*' => {of=>'str*'}],
+            req => 1,
+            pos => 0,
+        },
+        precall_logger => {
+            summary => 'Supply custom precall logger',
+            schema  => 'code*',
+            description => <<'_',
+
+Code will be called when logging method call. Code will be given a hash argument
+%args containing these keys: `args` (arrayref, the original @_), `orig`
+(coderef, the original method), `name` (string, the fully-qualified method
+name).
+
+You can use this mechanism to do selective logging, preprocess log message, etc.
+
+_
+        },
+        postcall_logger => {
+            summary => 'Supply custom postcall logger',
+            schema  => 'code*',
+            description => <<'_',
+
+Just like precall_logger, but code will be called after method is call. Code
+will be given a hash argument %args containing these keys: `args` (arrayref, the
+original @_), `orig` (coderef, the original method), `name` (string, the
+fully-qualified method name), `result` (arrayref, the method result).
+
+You can use this mechanism to do selective logging, preprocess log message,
+etc.
+
+_
+        },
+    },
+    result_naked => 1,
+};
+sub add_logging_to_package {
+    my %args = @_;
+
+    my $packages = $args{packages} or die "Please specify 'packages'";
+    $packages = [$packages] unless ref($packages) eq 'ARRAY';
+
+    for my $package (@$packages) {
+
+        die "Invalid package name $package"
+            unless $package =~ /\A\w+(::\w+)*\z/;
+
+        # require module
+        unless (package_exists($package)) {
+            eval "use $package; 1" or die "Can't load $package: $@";
+        }
+
+        my $src;
+        # get the calling package symbol table name
+        {
+            no strict 'refs';
+            $src = \%{ $package . '::' };
+        }
+
+        # loop through all symbols in calling package, looking for subs
+        for my $symbol (keys %$src) {
+            # get all code references, make sure they're valid
+            my $sub = *{ $src->{$symbol} }{CODE};
+            next unless defined $sub and defined &$sub;
+
+            # save all other slots of the typeglob
+            my @slots;
+
+            for my $slot (qw( SCALAR ARRAY HASH IO FORMAT )) {
+                my $elem = *{ $src->{$symbol} }{$slot};
+                next unless defined $elem;
+                push @slots, $elem;
+            }
+
+            # clear out the source glob
+            undef $src->{$symbol};
+
+            # replace the sub in the source
+            $src->{$symbol} = sub {
+                my $logger;
+                my @args = @_;
+
+                $logger = $args{precall_logger} // \&_default_precall_logger;
+                $logger->(
+                    orig   => $sub,
+                    name   => "${package}::$symbol",
+                    args   => [@args],
+                );
+
+                my $wa = wantarray;
+                my @res;
+                if ($wa) {
+                    @res =  $sub->(@args);
+                } else {
+                    $res[0] = $sub->(@args);
+                }
+
+                $logger = $args{postcall_logger} // \&_default_postcall_logger;
+                $logger->(
+                    orig   => $sub,
+                    name   => "${package}::$symbol",
+                    args   => [@args],
+                    result => \@res,
+                );
+
+                if ($wa) {
+                    return @res;
+                } else {
+                    return $res[0];
+                }
+            };
+
+            # replace the other slot elements
+            for my $elem (@slots) {
+                $src->{$symbol} = $elem;
+            }
+        } # for $symbol
+
+    } # for $package
+
+    1;
+}
+
+1;
+# ABSTRACT: Add logging to package
+
+=head1 SYNOPSIS
+
+ use Log::Any::For::Package qw(add_logging_to_package);
+ add_logging_to_package(packages => [qw/My::Module My::Other::Module/]);
+ # now calls to your module functions are logged, by default at level 'trace'
+
+
+=head1 CREDITS
+
+Some code portion taken from L<Devel::TraceMethods>.
+
+
+=head1 SEE ALSO
+
+L<Log::Any::For::Class>
+
+=cut
